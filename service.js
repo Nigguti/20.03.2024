1fb8c9624182c703ffcf473d5c7a1d9416fc4118
Nigguti <164106348+Nigguti@users.noreply.github.com>
2024-03-22 12:31:09 +0000

    22-03

--- service.js
+++ service.js
@@ -15,14 +15,14 @@ class UsuarioService {
     }
 
     obterUsuarioPorId(id) {
-        return this.usuarios.find(usuario => usuario.id === id); 
+        return this.usuarios.find(usuario => usuario.id === id);
     }
 
-    usuarioUpdate(id, novosDados) {
+    usuarioUpdate(id, novosDadosUsers) {
         const index = this.usuarios.findIndex(usuario => usuario.id === id);
         if (index !== -1) {
-    
-            this.usuarios[index] = { ...this.usuarios[index], ...novosDados };
+
+            this.usuarios[index] = { ...this.usuarios[index], ...novosDadosUsers };
             return this.usuarios[index];
         }
         return null;
@@ -32,7 +32,7 @@ class UsuarioService {
     usuarioDelete(id) {
         const index = this.usuarios.findIndex(usuario => usuario.id === id);
         if (index !== -1) {
-        
+
             this.usuarios.splice(index, 1);
             return true;
         }
@@ -41,5 +41,91 @@ class UsuarioService {
 
     obterUsuarios() {
         return this.usuarios;
+    }
+}
+
+class CategoriaService {
+    constructor() {
+        this.categorias = [];
+    }
+}
+
+criarCategoria(id, nome, cor); {
+    const novaCategoria = {
+        id,
+        nome,
+        cor
+    }
+    this.categoria.push(novaCategoria);
+    return novaCategoria;
+}
+
+categoriaAll(id); {
+    return categorias;
+}
+
+atualizarCategoria(id, novosDadosCategoria) {
+    const index = this.categorias.findIndex(categoria => categoria.id === id);
+    if (index !== -1) {
+        this.categorias[index] = { ...this.categorias[index], ...novosDadosCategoria };
+        return this.categorias[index];
+    }
+    return null;
+}
+
+excluirCategoria(id); {
+    const index = this.categorias.findIndex(categoria => categoria.id === id);
+    if (index !== -1) {
+        this.categorias.splice(index, 1);
+        return true;
+    }
+    return false;
+}
+
+class TarefaService {
+    constructor() {
+        this.tarefas = [];
+    }
+
+    criarTarefa(id, titulo, descricao, dataCriacao, dataConclusao, tipo, categoria, status, usuarioAssociado) {
+        const novaTarefa = {
+            id,
+            titulo,
+            descricao,
+            dataCriacao,
+            dataConclusao,
+            tipo,
+            categoria,
+            status,
+            usuarioAssociado
+        };
+        this.tarefas.push(novaTarefa);
+        return novaTarefa;
+    }
+
+    obterTodasTarefas() {
+        return this.tarefas;
+    }
+
+    obterTarefaPorId(id) {
+        return this.tarefas.find(tarefa => tarefa.id === id);
+    }
+
+    atualizarTarefa(id, novosDadosTarefa) {
+        const index = this.tarefas.findIndex(tarefa => tarefa.id === id);
+        if (index !== -1) {
+            this.tarefas[index] = { ...this.tarefas[index], ...novosDadosTarefa };
+            return this.tarefas[index];
+        }
+        return null;
+    }
+
+    excluirTarefa(id) {
+        const index = this.tarefas.findIndex(tarefa => tarefa.id === id);
+        if (index !== -1) {
+            this.tarefas.splice(index, 1);
+            return true;
+        }
+        return false;
     }
 }
