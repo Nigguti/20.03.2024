131a5aa634b8c939abc6251ab0925428bad4bdf7
Nigguti <164106348+Nigguti@users.noreply.github.com>
2024-03-27 11:58:56 +0000

    express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-teste
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Site Rodando");
+});
+
+app.get("/cadastro", function(res, req){
+    res.send("Cadastro do Usuário");
+    res.send("Login: --- ");
+    res.send("senha: ----");
+});
+
+app.get("/login", function(res, req){
+    res.send("Login: ---");
+    res.send("Senha: ----");
+})
+
+app.listen(3030, function () {
+    console.log("Rodando: http://localhost:3030");
+});
